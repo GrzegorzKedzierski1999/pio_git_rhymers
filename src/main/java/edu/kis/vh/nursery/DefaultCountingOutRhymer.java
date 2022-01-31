d44f31898f557f040875096c6a9f99b538ea0153
GrzegorzKedzierski1999 <kedzierskigrzegorz1999@gmail.com>
2022-01-31 18:33:21 +0100

    1.4 accesory 2 - dodanie gettera oraz settera dla pola total klasy DefaultCountingOutRhymer

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -10,6 +10,14 @@ public class DefaultCountingOutRhymer {
 
     private int total = EMPTY_TOTAL;
 
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
     protected void countIn(int in) {
         if (!isFull())
             numbers[++total] = in;
