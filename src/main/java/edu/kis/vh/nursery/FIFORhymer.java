8f5675dcfb8ffd1fd56e1326fb3b4d14759859fc
GrzegorzKedzierski1999 <kedzierskigrzegorz1999@gmail.com>
2022-01-31 19:01:40 +0100

    1.5.6 - wygenerowanie komentarzy dokumentacyjnych do klasy FIFORhymer

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,13 +1,26 @@
 package edu.kis.vh.nursery;
 
+/**
+ * Creates FIFO implementation of CountingOutRhymer
+ */
 public class FIFORhymer extends DefaultCountingOutRhymer {
 
+    /**
+     * instance of DefaultCountingOutRhymer
+     */
     private final DefaultCountingOutRhymer dRhymer = new DefaultCountingOutRhymer();
 
+    /**
+     * @return instance of DefaultCountingOutRhymer
+     */
     public DefaultCountingOutRhymer getDRhymer() {
         return dRhymer;
     }
 
+    /**
+     * Counts out first element of rhymer
+     * @return first element
+     */
     @Override
     public int countOut() {
         while (!callCheck())
